95861fc7e647e8f588216fa9a753b9ae24051d38
Haider Rasool <hyder.lyger@gmail.com>
2017-08-06 15:39:13 +0500

    project initiated

---
+++ src/pages/home/home.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  avatar: string;
+  userName: string = "Haider";
+  loading: any;
+  chatId: any = "123";
+  threadMessages: any;
+  userIds: any;
+  socketHost: string = "API.URL";
+  socket: any;
+  zone: any;
+  messages: Array<Object>;
+  chatInput: string = '';
+  onlineUsers: Array<number>;
+  user: any;      // active user
+  opponentId: any;
+  showError: boolean = false;
+  limit: number = 20;
+  offset: number = 0;
+  noOlder: boolean = false;
+  constructor(public navCtrl: NavController) {
+
+  }
+  sendChatMessage(chatInput){
+    alert("send");
+  }
+}
